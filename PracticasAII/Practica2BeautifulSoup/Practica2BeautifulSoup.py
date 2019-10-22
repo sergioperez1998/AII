ec9355869f520f5c341d4a6956182f7b04f16c1b
jeselirod <39128879+jeselirod@users.noreply.github.com>
2019-10-22 12:30:29 +0200

    Funcion almacenar completa y spinbox de marcas a medio hacer

--- PracticasAII/Practica2BeautifulSoup/Practica2BeautifulSoup.py
+++ PracticasAII/Practica2BeautifulSoup/Practica2BeautifulSoup.py
@@ -5,6 +5,7 @@ import re
 import datetime
 from tkinter import *
 import sqlite3
+from tkinter import messagebox
 
 def obtencionDatos():
     
@@ -43,19 +44,16 @@ def obtencionDatos():
             precioSinOferta= m.groups(1)[0]
         listadoPrecioSinOferta.append(precioSinOferta)
     
-    print(listadoMarcas)
-    print(listadoLinks)
-    print(listadoNombres)
-    print(listadoPrecioActual)
-    print(listadoPrecioSinOferta)
+    return listadoMarcas,listadoNombres,listadoLinks,listadoPrecioActual,listadoPrecioSinOferta
+   
            
 def ventanaPrincipal():
     top = Tk()
 
-    almacenarDatos = Button(top, text="Almacenar datos", fg = "Green", command = obtencionDatos)
+    almacenarDatos = Button(top, text="Almacenar datos", fg = "Green", command = almacenar_bd)
     almacenarDatos.pack(side = LEFT)
 
-    mostrarMarca = Button(top, text="Mostrar marca", fg = "Purple", command = obtenerMarca)
+    mostrarMarca = Button(top, text="Mostrar marca", fg = "Purple", command = seleccionar_marca)
     mostrarMarca.pack(side = LEFT)
 
     buscarOfertas = Button(top, text="Buscar ofertas", fg = "Blue", command = obtenerOferta)
@@ -93,6 +91,78 @@ def obtenerOferta():
     scrollbar.config(command = listbox.yview)
 
     top.mainloop()
+def almacenar_bd():
+    
+    conn = sqlite3.connect('PRACTICA2.db')
+    conn.text_factory = str  # para evitar problemas con el conjunto de caracteres que maneja la BD
+    conn.execute("DROP TABLE IF EXISTS ALIMENTOS")   
+    conn.execute('''CREATE TABLE ALIMENTOS
+       (ID INTEGER PRIMARY KEY  AUTOINCREMENT,
+        MARCA           TEXT    NOT NULL,
+        NOMBRE           TEXT    NOT NULL,
+        LINK           TEXT NOT NULL,
+        PRECIOACTUAL        TEXT   NOT NULL,
+        PRECIOSINOFERTA         TEXT    );''')
+    
+    l = obtencionDatos()
+    
+    i=0
+    marca=l[0]
+    nombre=l[1]
+    link=l[2]
+    precioActual=l[3]
+    precioSinOferta=l[4]
+   
+    
+    while i < len(marca):
+        
+        conn.execute("""INSERT INTO ALIMENTOS (MARCA, NOMBRE, LINK, PRECIOACTUAL, PRECIOSINOFERTA) VALUES (?,?,?,?,?)""",(marca[i],nombre[i],link[i], precioActual[i], precioSinOferta[i]))
+        i=i+1
+
+    conn.commit()
+    cursor = conn.execute("SELECT COUNT(*) FROM ALIMENTOS")
+    messagebox.showinfo( "Base Datos", "Base de datos creada correctamente \nHay " + str(cursor.fetchone()[0]) + " registros")
+    conn.close()
+    
+def seleccionar_marca():
+    l = obtencionDatos()
+    marcas=l[0]
+    master = Tk()
+    marca = Spinbox(master, values=marcas)
+    marca.pack()
+    boton1=Button(master, text="Seleccionar", command=listar_bd(marca.get()))
+    boton1.pack(side = LEFT)
+    master.mainloop()
+    
+
+def listar_bd(marca):
+        
+          
+        conn = sqlite3.connect('PRACTICA2.db')
+        conn.text_factory = str  
+        
+        cursor = conn.execute("""SELECT NOMBRE,PRECIOACTUAL FROM ALIMENTOS WHERE MARCA = ?""",(marca,))
+        imprimir_etiqueta(cursor)
+        conn.close()
+    
+    
+    
+    
+    
+def imprimir_etiqueta(cursor):
+    v = Toplevel()
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, width=150, yscrollcommand=sc.set)
+    for row in cursor:
+        lb.insert(END,row[0])
+        lb.insert(END,row[1])
+        lb.insert(END,'')
+    lb.pack(side = LEFT, fill = BOTH)
+    sc.config(command = lb.yview)
+
 
 if __name__ == "__main__":
     ventanaPrincipal()
+    
+
