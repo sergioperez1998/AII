de628f310e7210942a5081cd0503dad469274562
sergioperez1998 <sergio-pm@hotmail.com>
2019-11-11 17:11:10 +0100

    scraping practica 2 whoosh terminado

--- PracticasAII/Practica1Woosh/escrituraFichero.py
+++ PracticasAII/Practica1Woosh/escrituraFichero.py
@@ -4,47 +4,50 @@ import os
 
 def escrituraFichero():
     
-    datos= EjercicioWoosh.obtenerDatos()
+    datos= EjercicioWoosh.obtenerDatos           
+         
+    titulos=datos(0)
+    print(titulos)
+    enlaces=datos(1)
+    print(enlaces)
+    nombres=datos(2)
+    print(nombres)
+    fechas=datos(3)
+    print(fechas)
+    numeroRespuestas=datos(4)
+    print(numeroRespuestas)
+    numeroVisitas=datos(5)
+    print(numeroVisitas)
+    contadorLinea=0
     
-    for contenidoDatos in datos:
+    while contadorLinea < len(titulos):
         
-        i=1
-        archivo = open("Desktop/hola.txt","w")
-        #archivo = open("Desktop\\"+str(i)+".txt","r+")
-        #archivo = open("Temas\\"+str(i)+".txt","r+")
-        titulos=contenidoDatos[0]
-        enlaces=contenidoDatos[1]
-        nombres=contenidoDatos[2]
-        fechas=contenidoDatos[3]
-        numeroRespuestas=contenidoDatos[4]
-        numeroVisitas=contenidoDatos[5]
-        contadorLinea=0
-        while contadorLinea < len(titulos):
-            
-            archivo.write(titulos[contadorLinea]+ os.linesep)
-            archivo.write(enlaces[contadorLinea]+os.linesep)
-            archivo.write(nombres[contadorLinea]+os.linesep)
-            archivo.write(fechas[contadorLinea]+os.linesep)
-            archivo.write(numeroRespuestas[contadorLinea]+os.linesep)
-            archivo.write(numeroVisitas[contadorLinea]+os.linesep)
-            archivo.close()
-            
-            '''
-            archivo.write(titulos[contadorLinea]+"\n")
-            archivo.write(enlaces[contadorLinea]+"\n")
-            archivo.write(nombres[contadorLinea]+"\n")
-            archivo.write(fechas[contadorLinea]+"\n")
-            archivo.write(numeroRespuestas[contadorLinea]+"\n")
-            archivo.write(numeroVisitas[contadorLinea]+"\n")
-            '''
-            '''
-            pickle.dump(titulos[contadorLinea]+"\n",archivo)
-            pickle.dump(enlaces[contadorLinea]+"\n",archivo)
-            pickle.dump(nombres[contadorLinea]+"\n",archivo)
-            pickle.dump(fechas[contadorLinea]+"\n",archivo)
-            pickle.dump(numeroRespuestas[contadorLinea]+"\n",archivo)
-            pickle.dump(numeroVisitas[contadorLinea]+"\n",archivo)
-            '''
-            contadorLinea=contadorLinea+1
+        archivo = open("C:\\Users\\sergi\\Desktop\\Mi Equipo\\Facultad\\CUARTO CURSO\\ACCESO INTELIGENTE A LA INFORMACION\\REPO\\AII\\PracticasAII\\Practica1Woosh\\Temas\\"+str(contadorLinea+1)+".txt","w")
         
-        i=i+1
+        archivo.write(titulos[contadorLinea]+ "\n")
+        archivo.write(enlaces[contadorLinea]+"\n")
+        archivo.write(nombres[contadorLinea]+"\n")
+        archivo.write(fechas[contadorLinea]+"\n")
+        archivo.write(numeroRespuestas[contadorLinea]+"\n")
+        archivo.write(numeroVisitas[contadorLinea])
+        
+        
+        '''
+        archivo.write(titulos[contadorLinea]+"\n")
+        archivo.write(enlaces[contadorLinea]+"\n")
+        archivo.write(nombres[contadorLinea]+"\n")
+        archivo.write(fechas[contadorLinea]+"\n")
+        archivo.write(numeroRespuestas[contadorLinea]+"\n")
+        archivo.write(numeroVisitas[contadorLinea]+"\n")
+        '''
+        '''
+        pickle.dump(titulos[contadorLinea]+"\n",archivo)
+        pickle.dump(enlaces[contadorLinea]+"\n",archivo)
+        pickle.dump(nombres[contadorLinea]+"\n",archivo)
+        pickle.dump(fechas[contadorLinea]+"\n",archivo)
+        pickle.dump(numeroRespuestas[contadorLinea]+"\n",archivo)
+        pickle.dump(numeroVisitas[contadorLinea]+"\n",archivo)
+        '''
+        contadorLinea=contadorLinea+1
+        archivo.close()
+escrituraFichero()
---
+++ PracticasAII/Practica2Woosh/Woosh.py
@@ -0,0 +1,55 @@
+encoding = "utf-8"
+from bs4 import BeautifulSoup
+import urllib.request as urllib2
+import datetime
+from tkinter import *
+from tkinter import messagebox
+
+
+def llamadaObtencionDatos():
+    
+    i=1
+    lista=[]
+    
+    while i<4:
+        url="http://www.sensacine.com/noticias/?page="+str(i)
+        if i == 1:
+            lista=obtenerDatos(url)
+        else:
+            lista[0].extend(obtenerDatos(url)[0])
+            lista[1].extend(obtenerDatos(url)[1])
+            lista[2].extend(obtenerDatos(url)[2])
+            lista[3].extend(obtenerDatos(url)[3])
+            lista[4].extend(obtenerDatos(url)[4])
+        i=i+1
+     
+    return lista
+
+def obtenerDatos(url):
+    
+    urlBasica="http://www.sensacine.com/"
+    response = urllib2.urlopen(url)
+    webContent = response.read()
+    soup = BeautifulSoup(webContent, 'html.parser')
+    
+    listaCategoria=[]
+    listaTitulos=[]
+    listaEnlaces=[]
+    listaFechas=[]
+    listaDescripciones=[]
+    meses={"enero":"01", "febrero":"02", "marzo":"03", "abril":"04", "mayo":"05", "junio":"06", "julio":"07", "agosto":"08", "septiembre":"09", "octubre":"10", "noviembre":"11", "diciembre":"12" }
+    
+    for categorias in soup.findAll("div",attrs={"class":"meta-category"}):
+        listaCategoria.append(categorias.string.split("-")[1].strip(" "))
+    for titulos in soup.findAll("a",attrs={"class":"meta-title-link"}):
+        listaTitulos.append(titulos.string.strip())
+        listaEnlaces.append(urlBasica+titulos.get("href"))
+    for fechas in soup.findAll("div",attrs={"class":"meta-date"}):
+        fechaSinCasting=fechas.string.split(" ")[1]+ "/" + meses[fechas.string.split(" ")[3]] + "/" + fechas.string.split(" ")[5]
+        fechasCasting= datetime.datetime.strptime(fechaSinCasting, '%d/%m/%Y')
+        listaFechas.append(fechasCasting)
+    for descripciones in soup.findAll("div",attrs={"class":"meta-body"}):
+        listaDescripciones.append(descripciones.string)
+        
+    return listaCategoria, listaTitulos, listaEnlaces, listaFechas ,listaDescripciones
+        
