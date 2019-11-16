9c24450e2f3ad9c801f2ae3599b8ae8ad85344b9
jeselirod <39128879+jeselirod@users.noreply.github.com>
2019-11-16 19:05:32 +0100

    Todo copiado de mi rarma(jeselirod)

--- PracticasAII/Practica2Woosh/Woosh.py
+++ PracticasAII/Practica2Woosh/Woosh.py
@@ -1,4 +1,4 @@
-encoding = "utf-8"
+from datetime import datetime as datetime2
 from bs4 import BeautifulSoup
 import urllib.request
 import datetime
@@ -9,6 +9,12 @@ from whoosh.analysis import StemmingAnalyzer
 import os, os.path
 from whoosh import index,fields
 import errno
+import os
+from whoosh.index import create_in,open_dir
+from whoosh.fields import Schema, TEXT, DATETIME, ID
+from whoosh.qparser import QueryParser, MultifieldParser
+from whoosh import qparser
+from whoosh.qparser.dateparse import DateParserPlugin
 
 
 
@@ -37,7 +43,7 @@ def obtenerDatos(url):
     urlBasica="http://www.sensacine.com/"
     response = urllib.request.urlopen(url)
     
-    soup = BeautifulSoup(response.read().decode("latin-1"), 'lxml')
+    soup = BeautifulSoup(response.read().decode("latin-1"), 'html.parser')
     
     listaCategoria=[]
     listaTitulos=[]
@@ -57,108 +63,13 @@ def obtenerDatos(url):
             listaFechas.append(fechasCasting)
         for descripciones in soup.findAll("div",attrs={"class":"meta-body"}):
             listaDescripciones.append(descripciones.string)
+    
+   
         
     return listaCategoria, listaTitulos, listaEnlaces, listaFechas ,listaDescripciones
 
 
-def ventanaDatos():
-    dirdocs="Txts"
-    dirindex="Index"
-    top = Tk()
-
-    cargarDatos = Button(top, text="Cargar", command = lambda: (dirdocs,dirindex))
-    cargarDatos.pack(side = LEFT)
-
-    salir = Button(top, text= "Salir", command = quit)
-    salir.pack(side = RIGHT)
-
-    top.mainloop()
-
-def ventanaBuscar():
-    top = Tk()
-
-    tituloYDescripcion = Button(top, text="Titulo y Descripcion", command = ventanaTituloYDescripcion)
-    tituloYDescripcion.pack(side = LEFT)
-
-    fecha = Button(top, text="Fecha", command = ventanaFecha)
-    fecha.pack(side = LEFT)
-
-    descripcion = Button(top, text="Descripcion", command = ventanaDescripcion)
-    descripcion.pack(side = RIGHT)
-
-    top.mainloop()
-
-def ventanaTituloYDescripcion():
-    def buscarNoticias(event):
-        v = Toplevel()
-        sc = Scrollbar(v)
-        sc.pack(side=RIGHT, fill=Y)
-        lb = Listbox(v, width=150, yscrollcommand=sc.set)
-        lb.pack(side = LEFT, fill = BOTH)
-        sc.config(command = lb.yview)
-
-        s = entry.get() #s es lo que escribe el usuario
-        
-    top = Tk()
-
-    L1 = Label(top, text="Introduzca una o varias palabras:")
-    L1.pack(side=LEFT)
-    entry = Entry(top, bd = 5)
-    entry.bind("<Return>", buscarNoticias)
-    entry.pack(side = RIGHT)
-
-    top.mainloop()
-
-def ventanaFecha():
-    def buscarNoticiasFecha():
-        v = Toplevel()
-        sc = Scrollbar(v)
-        sc.pack(side=RIGHT, fill=Y)
-        lb = Listbox(v, width=150, yscrollcommand=sc.set)
-        lb.pack(side = LEFT, fill = BOTH)
-        sc.config(command = lb.yview)
-
-        s1 = entry1.get()
-        s2 = entry2.get() #s1 y s2 son los valores de las fechas en formato DD/MM/AAAA
-        
-    top = Tk()
-
-    L1 = Label(top, text="Introduzca un rango de fechas en formato DD/MM/AAAA:")
-    L2 = Label(top, text="-")
-    L1.pack(side=LEFT)
-    entry1 = Entry(top, bd = 5)
-    entry2 = Entry(top, bd = 5)
-    button = Button(top, text = "Buscar", command = buscarNoticiasFecha)
-    entry1.pack(side = LEFT)
-    L2.pack(side = LEFT)
-    entry2.pack(side = LEFT)
-    button.pack(RIGHT)
-
-    top.mainloop()
-
-def ventanaDescripcion():
-    def buscarNoticias(event):
-        v = Toplevel()
-        sc = Scrollbar(v)
-        sc.pack(side=RIGHT, fill=Y)
-        lb = Listbox(v, width=150, yscrollcommand=sc.set)
-        lb.pack(side = LEFT, fill = BOTH)
-        sc.config(command = lb.yview)
-
-        s = entry.get() #s es lo que escribe el usuario
-        
-    top = Tk()
-
-    L1 = Label(top, text="Introduzca una frase:")
-    L1.pack(side=LEFT)
-    entry = Entry(top, bd = 5)
-    entry.bind("<Return>", buscarNoticias)
-    entry.pack(side = RIGHT)
-
-    top.mainloop()
-        
 def crearTxt():
-    
     lista = llamadaObtencionDatos()
     
     try:
@@ -169,7 +80,7 @@ def crearTxt():
     
     for i in range(0,len(lista[0])):
 
-        file_object = open("Documentos\\Archivo"+str(i+1)+".txt","w",encoding="utf-8")
+        file_object = open("Documentos\\Archivo"+str(i+1)+".txt","w",encoding='utf-8')
         file_object.write(str(lista[0][i]))
         file_object.write("\n")
         file_object.write(str(lista[1][i]))
@@ -180,18 +91,216 @@ def crearTxt():
         file_object.write("\n")
         a = lista[4][i]
         b = str(a)
-        file_object.write(b)        
+        file_object.write(b)
+
+
+
+
+
+        
+def apartado_a(dirdocs,dirindex):
+    if not os.path.exists(dirdocs):
+        print ("Error: no existe el directorio de documentos " + dirdocs)
+    else:
+        if not os.path.exists(dirindex):
+            os.mkdir(dirindex)
+
+    ix = create_in(dirindex, schema=get_schema())
+    writer = ix.writer()
+    i=0
+    for docname in os.listdir(dirdocs):
+        if not os.path.isdir(dirdocs+docname):
+            add_doc(writer, dirdocs, docname)
+            i+=1
+    messagebox.showinfo("Fin de indexado", "Se han indexado "+str(i)+ " correos")
+            
+    writer.commit()
+
+
+            
+            
+def apartado_b_a(dirindex):
+    def mostrar_lista(event):
+        ix=open_dir(dirindex)      
+        with ix.searcher() as searcher:
+            query = QueryParser("titulo", ix.schema ).parse(str(en.get())) | QueryParser("descripcion", ix.schema ).parse(str(en.get()))
+            results = searcher.search(query)
+            imprimir_b_a(results)
     
-def ventana_principal():
+    v = Toplevel()
+    v.title("Busqueda por titulo  y descripcion")
+    f =Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca una o varias palabras  :")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", mostrar_lista)
+    en.pack(side=LEFT)
+      
+
+def imprimir_b_a(results):
+    v = Toplevel()
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, width=150, yscrollcommand=sc.set)
+    for r in results:
+        lb.insert(END,r['categoria'])
+        lb.insert(END,r['titulo'])
+        lb.insert(END,r['fecha'])
+        lb.insert(END,'')
+    lb.pack(side = LEFT, fill = BOTH)
+    sc.config(command = lb.yview)  
+
+
+def apartado_b_b(dirindex):
+    def buscarNoticiasFecha(event):
+        
+        ix=open_dir(dirindex)      
+        with ix.searcher() as searcher:
+            try:
+                fecha = str(en.get())
+                fecha1 = fecha.split("-")[0]
+                fecha1Consulta = fecha1.split("/")[2]+fecha1.split("/")[1]+fecha1.split("/")[0] 
+                fecha2 = fecha.split("-")[1]
+                fecha2Consulta = fecha2.split("/")[2]+fecha2.split("/")[1]+fecha2.split("/")[0] 
+                myquery='['+ fecha1Consulta + ' to ' +fecha2Consulta+']'
+                print(myquery)
+                query = QueryParser("fecha", ix.schema).parse(myquery)
+           
+                results = searcher.search(query)
+                imprimir_b_b(results)
+            except:
+                print ("Error: Formato de fecha incorrecto")
+                
+    
+    v = Toplevel()
+    v.title("Busqueda rango de fechas ")
+    f =Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca rango de fechas (DD/MM/AAAA-DD/MM/AAAA):")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", buscarNoticiasFecha)
+    en.pack(side=LEFT)
+    
+  
+def imprimir_b_b(results):
+    v = Toplevel()
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, width=150, yscrollcommand=sc.set)
+    for r in results:
+        lb.insert(END,r['titulo'])
+        lb.insert(END,r['fecha'])
+        lb.insert(END,'')
+    lb.pack(side = LEFT, fill = BOTH)
+    sc.config(command = lb.yview)  
+    
+def apartado_b_c(dirindex):
+    def mostrar_lista(event):
+        ix=open_dir(dirindex)      
+        with ix.searcher() as searcher:
+            query = QueryParser("descripcion", ix.schema ).parse(str(en.get())) 
+            results = searcher.search(query)
+            imprimir_b_c(results)
+
+
+    v = Toplevel()
+    v.title("Busqueda por descripcion")
+    f =Frame(v)
+    f.pack(side=TOP)
+    l = Label(f, text="Introduzca una frase :")
+    l.pack(side=LEFT)
+    en = Entry(f)
+    en.bind("<Return>", mostrar_lista)
+    en.pack(side=LEFT)
+      
+def imprimir_b_c(results):
+    v = Toplevel()
+    sc = Scrollbar(v)
+    sc.pack(side=RIGHT, fill=Y)
+    lb = Listbox(v, width=150, yscrollcommand=sc.set)
+    for r in results:
+        lb.insert(END,r['titulo'])
+        lb.insert(END,r['enlace'])
+        lb.insert(END,r['descripcion'])
+        lb.insert(END,'')
+    lb.pack(side = LEFT, fill = BOTH)
+    sc.config(command = lb.yview)  
+    
+   
+   
+          
+def get_schema():
+    return Schema(categoria=TEXT(stored=True), titulo=TEXT(stored=True), enlace=TEXT(stored=True),fecha=DATETIME(stored=True), descripcion=TEXT(stored=True),nombrefichero=ID(stored=True) )
+
+
+def add_doc(writer, path, docname):
+    try:    
+        fileobj=open(path+'\\'+docname, "r")
+        cate=fileobj.readline().strip()
+        tit=fileobj.readline().strip()
+        enl=fileobj.readline().strip()
+        f=fileobj.readline().strip()
+        fe=datetime2.strptime(f,'%Y-%m-%d %H:%M:%S')
+        descrip=fileobj.read()
+        fileobj.close()           
+      
+        if descrip==None:
+            writer.add_document(categoria=cate,titulo=tit, enlace=enl, descripcion=descrip, nombrefichero=docname)
+        else:
+            writer.add_document(categoria=cate,titulo=tit, enlace=enl, fecha=fe, descripcion=descrip, nombrefichero=docname)
+            
+            
+        writer.add_document(categoria=cate,titulo=tit, enlace=enl, fecha=fe, descripcion=descrip, nombrefichero=docname)
+          
+        print("Creado indice para fichero " + docname)
+    except:
+        
+        print("Error: No se ha podido anadir el documento "+path+'\\'+docname)
+        
+          
+        
+        
+    
+def ventanaDatos():
+    dirdocs="Documentos\\"
+    dirindex="Index"
     top = Tk()
-    datos = Button(top, text="Datos", command = ventanaDatos)
-    datos.pack(side = LEFT)
-    buscar = Button(top, text="Buscar", command = ventanaBuscar)
-    buscar.pack(side = RIGHT)
+    indexar = Button(top, text="Cargar", command = lambda: apartado_a(dirdocs,dirindex))
+    indexar.pack(side = TOP)
+    salir = Button(top, text= "Salir", command = quit)
+    salir.pack(side = RIGHT)
     top.mainloop()
 
+def ventanaBuscar():
+    top = Tk()
+    dirindex="Index"
+
+    Buscar = Button(top, text="Buscar por titulo ", command = lambda: apartado_b_a(dirindex))
+    Buscar.pack(side = TOP)
+
+    fecha = Button(top, text="Fecha", command = lambda: apartado_b_b(dirindex))
+    fecha.pack(side = LEFT)
+    Buscar = Button(top, text="Buscar por descripcion ", command = lambda: apartado_b_c(dirindex))
+    Buscar.pack(side = TOP)
+
+
+    top.mainloop()
+   
+
+    
+def ventanaPrincipal():
+    top = Tk()
+
+    almacenarDatos = Button(top, text="Datos", command = ventanaDatos)
+    almacenarDatos.pack(side = LEFT)
+
+    buscarOfertas = Button(top, text="Buscar", command = ventanaBuscar)
+    buscarOfertas.pack(side = RIGHT)
+
+    top.mainloop()
+
+
 if __name__ == '__main__':
-    ventana_principal() 
-
-
-crearTxt()
+    ventanaPrincipal()
