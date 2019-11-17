cf010ca0d55b85670563265b5eb07a0b0584d8dd
sergioperez1998 <sergio-pm@hotmail.com>
2019-11-17 12:21:12 +0100

    arreglado el tema de tildes

--- PracticasAII/Practica2Woosh/Woosh.py
+++ PracticasAII/Practica2Woosh/Woosh.py
@@ -8,9 +8,15 @@ from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
 from whoosh.analysis import StemmingAnalyzer
 import os, os.path
 from whoosh import index,fields
+from unicodedata import normalize
 import errno
 
-
+def eliminadorDiacriticos(cadena):
+    
+    s = re.sub(r"([^n\u0300-\u036f]|n(?!\u0303(?![\u0300-\u036f])))[\u0300-\u036f]+", r"\1", 
+    normalize("NFD", cadena), 0, re.I)
+    s = normalize('NFC', s)
+    return s
 
 
 def llamadaObtencionDatos():
@@ -49,113 +55,23 @@ def obtenerDatos(url):
     for categorias in soup.findAll("div",attrs={"class":"meta-category"}):
         listaCategoria.append(categorias.string.split("-")[1].strip(" "))
         for titulos in soup.findAll("a",attrs={"class":"meta-title-link"}):
-            listaTitulos.append(titulos.string.strip())
+            TituloSinParsear=titulos.string.strip()
+            TituloParseado = eliminadorDiacriticos(TituloSinParsear)
+            listaTitulos.append(TituloParseado)
             listaEnlaces.append(urlBasica+titulos.get("href"))
         for fechas in soup.findAll("div",attrs={"class":"meta-date"}):
             fechaSinCasting=fechas.string.split(" ")[1]+ "/" + meses[fechas.string.split(" ")[3]] + "/" + fechas.string.split(" ")[5]
             fechasCasting= datetime.datetime.strptime(fechaSinCasting, '%d/%m/%Y')
             listaFechas.append(fechasCasting)
         for descripciones in soup.findAll("div",attrs={"class":"meta-body"}):
-            listaDescripciones.append(descripciones.string)
+            DescripcionSinParsear= descripciones.string
+            if "NoneType" in type(DescripcionSinParsear).__name__:
+                listaDescripciones.append(DescripcionSinParsear)
+            else:
+                DescripcionParseada = eliminadorDiacriticos(DescripcionSinParsear)
+                listaDescripciones.append(DescripcionParseada)
         
     return listaCategoria, listaTitulos, listaEnlaces, listaFechas ,listaDescripciones
-
-
-def ventanaDatos():
-    dirdocs="Txts"
-    dirindex="Index"
-    top = Tk()
-
-    cargarDatos = Button(top, text="Cargar", command = lambda: (dirdocs,dirindex))
-    cargarDatos.pack(side = LEFT)
-
-    salir = Button(top, text= "Salir", command = quit)
-    salir.pack(side = RIGHT)
-
-    top.mainloop()
-
-def ventanaBuscar():
-    top = Tk()
-
-    tituloYDescripcion = Button(top, text="Titulo y Descripcion", command = ventanaTituloYDescripcion)
-    tituloYDescripcion.pack(side = LEFT)
-
-    fecha = Button(top, text="Fecha", command = ventanaFecha)
-    fecha.pack(side = LEFT)
-
-    descripcion = Button(top, text="Descripcion", command = ventanaDescripcion)
-    descripcion.pack(side = RIGHT)
-
-    top.mainloop()
-
-def ventanaTituloYDescripcion():
-    def buscarNoticias(event):
-        v = Toplevel()
-        sc = Scrollbar(v)
-        sc.pack(side=RIGHT, fill=Y)
-        lb = Listbox(v, width=150, yscrollcommand=sc.set)
-        lb.pack(side = LEFT, fill = BOTH)
-        sc.config(command = lb.yview)
-
-        s = entry.get() #s es lo que escribe el usuario
-        
-    top = Tk()
-
-    L1 = Label(top, text="Introduzca una o varias palabras:")
-    L1.pack(side=LEFT)
-    entry = Entry(top, bd = 5)
-    entry.bind("<Return>", buscarNoticias)
-    entry.pack(side = RIGHT)
-
-    top.mainloop()
-
-def ventanaFecha():
-    def buscarNoticiasFecha():
-        v = Toplevel()
-        sc = Scrollbar(v)
-        sc.pack(side=RIGHT, fill=Y)
-        lb = Listbox(v, width=150, yscrollcommand=sc.set)
-        lb.pack(side = LEFT, fill = BOTH)
-        sc.config(command = lb.yview)
-
-        s1 = entry1.get()
-        s2 = entry2.get() #s1 y s2 son los valores de las fechas en formato DD/MM/AAAA
-        
-    top = Tk()
-
-    L1 = Label(top, text="Introduzca un rango de fechas en formato DD/MM/AAAA:")
-    L2 = Label(top, text="-")
-    L1.pack(side=LEFT)
-    entry1 = Entry(top, bd = 5)
-    entry2 = Entry(top, bd = 5)
-    button = Button(top, text = "Buscar", command = buscarNoticiasFecha)
-    entry1.pack(side = LEFT)
-    L2.pack(side = LEFT)
-    entry2.pack(side = LEFT)
-    button.pack(RIGHT)
-
-    top.mainloop()
-
-def ventanaDescripcion():
-    def buscarNoticias(event):
-        v = Toplevel()
-        sc = Scrollbar(v)
-        sc.pack(side=RIGHT, fill=Y)
-        lb = Listbox(v, width=150, yscrollcommand=sc.set)
-        lb.pack(side = LEFT, fill = BOTH)
-        sc.config(command = lb.yview)
-
-        s = entry.get() #s es lo que escribe el usuario
-        
-    top = Tk()
-
-    L1 = Label(top, text="Introduzca una frase:")
-    L1.pack(side=LEFT)
-    entry = Entry(top, bd = 5)
-    entry.bind("<Return>", buscarNoticias)
-    entry.pack(side = RIGHT)
-
-    top.mainloop()
         
 def crearTxt():
     
@@ -180,18 +96,6 @@ def crearTxt():
         file_object.write("\n")
         a = lista[4][i]
         b = str(a)
-        file_object.write(b)        
-    
-def ventana_principal():
-    top = Tk()
-    datos = Button(top, text="Datos", command = ventanaDatos)
-    datos.pack(side = LEFT)
-    buscar = Button(top, text="Buscar", command = ventanaBuscar)
-    buscar.pack(side = RIGHT)
-    top.mainloop()
-
-if __name__ == '__main__':
-    ventana_principal() 
-
+        file_object.write(b)
 
 crearTxt()
