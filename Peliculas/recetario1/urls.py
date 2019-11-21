4f3b1f37bfcb3838884f85696fe3d048ec64dc94
jeselirod <39128879+jeselirod@users.noreply.github.com>
2019-11-21 12:11:53 +0100

    EjerciciosPeliculas Django

---
+++ Peliculas/recetario1/urls.py
@@ -0,0 +1,20 @@
+"""recetario1 URL Configuration
+
+
+"""
+from django.contrib import admin
+from django.urls import path, re_path
+from django.conf import settings
+from django.views import static
+from principal import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    re_path(r'media/(?P<path>.*)', static.serve,
+            {'document_root': settings.MEDIA_ROOT,}),
+    path('sobre/',views.sobre),
+    path('usuarios/', views.usuarios),
+    path('recetas/', views.lista_recetas),
+    re_path(r'receta/(?P<id_receta>\d+)',views.detalle_receta),
+    path('',views.inicio),
+    ]
