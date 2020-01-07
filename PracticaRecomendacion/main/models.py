934a9ef70929c2c1e5142a35ce284c138502b04b
sergioperez1998 <sergio-pm@hotmail.com>
2020-01-07 19:05:23 +0100

    modelo

--- PracticaRecomendacion/main/models.py
+++ PracticaRecomendacion/main/models.py
@@ -1,3 +1,45 @@
 from django.db import models
 
 # Create your models here.
+class Artirta(models.Model):
+    
+    id=models.IntegerField(primary_key=True, null=False)
+    name=models.TextField(verbose_name='Nombre', unique=True)
+    url=models.URLField(verbose_name = 'Url del artista')
+    pictureUrl=models.URLField(verbose_name = 'Url de la imagen')
+    def __str__(self):
+        return self.name
+        
+class Etiqueta(models.Model):
+    
+    id=models.IntegerField(primary_key=True, null=False)
+    tagValue=models.TextField(verbose_name='Valor de la tag', unique=True)
+    def __str__(self):
+        return self.tagValue
+
+class Usuario(models.Model):
+    idUsuario=models.IntegerField(primary_key=True, null=False)
+    def __str__(self):
+        return self.idUsuario
+        
+class UsuarioArtista(models.Model):
+    
+    idUsuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    idArtista=models.ForeignKey(Artirta, on_delete=models.CASCADE)
+    tiempoEscucha=models.IntegerField()
+    def __str__(self):
+        return self.tiempoEscucha
+
+class UsuarioEtiquetaArtista(models.Model):
+    
+    idUsuario=models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    idArtista=models.ForeignKey(Artirta, on_delete=models.CASCADE)
+    idTag=models.ForeignKey(Etiqueta, on_delete=models.CASCADE)
+    fecha=models.DateField(null=False, blank=False)
+    def __str__(self):
+        return self.fecha
+    
+
+        
+    
+    
