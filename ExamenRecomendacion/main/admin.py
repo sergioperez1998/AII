d50c8ab4c41f4b42e888ea4fed1d816397006564
sergioperez1998 <sergio-pm@hotmail.com>
2020-01-13 18:13:59 +0100

    Modelo terminado

--- ExamenRecomendacion/main/admin.py
+++ ExamenRecomendacion/main/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from main.models import Libro, Usuario, Puntuacion
 
 # Register your models here.
+admin.site.register(Libro)
+admin.site.register(Usuario)
+admin.site.register(Puntuacion)
--- ExamenRecomendacion/main/models.py
+++ ExamenRecomendacion/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
 
-# Create your models here.
+class Libro(models.Model):
+    bookId=models.AutoField(primary_key=True)
+    titulo=models.TextField(verbose_name='Titulo',unique=True)
+    autor=models.TextField(verbose_name='Autor')
+    genero=models.TextField(verbose_name='Genero')
+    idioma=models.TextField(verbose_name='Idioma')
+    puntuaciones= models.PositiveSmallIntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    def __str__(self):
+        return self.titulo
+    
+class Usuario(models.Model):
+    idUsuario=models.TextField(primary_key=True)
+    def __str__(self):
+        return str(self.idUsuario)
+    
+class Puntuacion(models.Model):
+    IdUsuario=models.ForeignKey(Usuario, on_delete=models.CASCADE, null=True)
+    bookId=models.ForeignKey(Libro, on_delete=models.CASCADE, null=True)
+    puntuacion= models.PositiveSmallIntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    def __str__(self):
+        return str(self.genreName)
+    
+
