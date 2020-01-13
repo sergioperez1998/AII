f817f82ea5725751d60eb1fde13b8d23b10437c4
jeselirod <39128879+jeselirod@users.noreply.github.com>
2020-01-13 12:39:28 +0100

    algunas configuraciones para el populate antes de empezar la practica

---
+++ ExamenRecomendacion/main/populate.py
@@ -0,0 +1,12 @@
+#encoding:utf-8
+
+from datetime import datetime
+path= "C:\\Users\\Usuario\\Desktop\\Universidad\\cuarto a�o\\AII\\Practicar resueltas\\PracticaRS\\ml-100k"
+
+
+
+def populateDatabase():
+    print("Finished database population")
+    
+if __name__ == '__main__':
+    populateDatabase()
--- ExamenRecomendacion/main/views.py
+++ ExamenRecomendacion/main/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from main.populate import populateDatabase
 
 # Create your views here.
+def index(request): 
+    return render(request,'index.html')
+def populateDB(request):
+    populateDatabase() 
+    return render(request,'populate.html')
